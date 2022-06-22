736e6a2e33727f9540b604f1de4514775ee9a623
Natalia Homusko <avgust_net85@mail.ru>
2022-06-22 15:20:44 +0300

    изменения для второго задания

--- README.md
+++ README.md
@@ -14,3 +14,4 @@ git config --global core.autocrlf true
 git init
 git status
 git add . 
+
--- index.js
+++ index.js
@@ -2,8 +2,17 @@
 
 const productName = 'яблоко';
 let producAmount = 20;
-const productCategory = 'food';
+const productCategory = 'продукты питания';
 let productCost = 44;
 
 console.log(productName);
 console.log('Общая стоимость яблок: ' + producAmount * productCost + 'руб.');
+
+const productName2 = 'кекс';
+const count2 = 8;
+const category2 = 'продукты питания';
+const price2 = 12;
+
+console.log(productName2);
+console.log(`общая сумма товара: ${count2 * price2}`);
+
