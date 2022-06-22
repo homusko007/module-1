f83733e184133cb59d3c5b67a3461172dc8caab5
Natalia Homusko <avgust_net85@mail.ru>
2022-06-22 20:45:58 +0300

    добавила отдельный блок инструкций

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-
+{
 const productName = 'яблоко';
 let producAmount = 20;
 const productCategory = 'продукты питания';
@@ -7,7 +7,9 @@ let productCost = 44;
 
 console.log(productName);
 console.log('Общая стоимость яблок: ' + producAmount * productCost + 'руб.');
+}
 
+{
 const productName2 = 'кекс';
 const count2 = 8;
 const category2 = 'продукты питания';
@@ -15,4 +17,4 @@ const price2 = 12;
 
 console.log(productName2);
 console.log(`общая сумма товара: ${count2 * price2}`);
-
+}
