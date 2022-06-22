8c71eafd2f38765d9cedfaabe65a5ccedf21ba8d
Natalia Homusko <avgust_net85@mail.ru>
2022-06-22 13:17:17 +0300

    initial

---
+++ index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const productName = 'яблоко';
+let producAmount = 20;
+const productCategory = 'food';
+let productCost = 44;
+
+console.log(productName);
+console.log('Общая стоимость яблок: ' + producAmount * productCost + 'руб.');
