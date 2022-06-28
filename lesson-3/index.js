ba092bb04910d5a4f93e64c13a712e8805cce85c
Natalia Homusko <100041021+homusko007@users.noreply.github.com>
2022-06-28 12:11:23 +0300

    Update index.js

--- lesson-3/index.js
+++ lesson-3/index.js
@@ -1,14 +1,34 @@
 'use strict';
+{
+    const productName = 'яблоко';
+    let producAmount = 20;
+    const productCategory = 'продукты питания';
+    let productCost = 44;
+
+    console.log(productName);
+    console.log('Общая стоимость яблок: ' + producAmount * productCost + 'руб.');
+}
 
 {
-    const productName = prompt('Ведите наименование товара');
-    const count = parseInt(prompt('Ведите количество товара'));
-    const category = prompt('Ведите категорию товара');
-    const price = parseInt(prompt('Ведите стоимость товара'));
+    const productName2 = 'кекс';
+    const count2 = 8;
+    const category2 = 'продукты питания';
+    const price2 = 12;
+
+    console.log(productName2);
+    console.log(`общая сумма товара: ${count2 * price2}`);
+}
 
 
-    console.log('count: ', typeof count);
-    console.log('price: ', typeof price);
+{
+    const productName3 = prompt('Ведите наименование товара');
+    const count3 = parseInt(prompt('Ведите количество товара'));
+    const category3 = prompt('Ведите категорию товара');
+    const price3 = parseInt(prompt('Ведите стоимость товара'));
 
-    console.log(`На складе ${count} единицы товара ${productName} на сумму ${count * price} руб.`);
+
+    console.log('count: ', typeof count3);
+    console.log('price: ', typeof price3);
+
+    console.log(`На складе ${count3} единицы товара ${productName3} на сумму ${count3 * price3} руб.`);
 }
