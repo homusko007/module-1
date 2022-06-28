dcbbf9cb50f911b691ce766d58961cd97139e954
Natalia Homusko <100041021+homusko007@users.noreply.github.com>
2022-06-28 12:10:32 +0300

    Update einstein.js

--- lesson-3/einstein.js
+++ lesson-3/einstein.js
@@ -4,8 +4,7 @@
     const myWeight = 50;
     const speedLight = 3e8;
 
-    const energy = myWeight * speedLight ** 2;
+    const energy = myWeight * (speedLight ** 2);
     console.log(`Энергия тела: ${energy}`);
-    
 }
 
