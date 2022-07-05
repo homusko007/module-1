ae8d442dcbe1829fa3ff40384bbf08d4ba6ba4ca
Natalia Homusko <100041021+homusko007@users.noreply.github.com>
2022-07-05 11:55:31 +0300

    Update task03.js

--- lesson-8/task03.js
+++ lesson-8/task03.js
@@ -9,8 +9,10 @@ const getArr3 = (lenght, n, m, condition) => {
     
       for (let i = 0; i < lenght; i++) {
         if (condition === 'even') {
-         arr3[i] = Math.floor(Math.random() * ((max - min - 1)) * 2 + min / 2);
-            
+          arr3[i] = Math.floor(Math.random() * (max - min) + min); 
+          if (arr3[i] % 2 != 0) {
+            ++arr3[i];
+          }
         } else {
          arr3[i] = Math.floor(Math.random() * ((max - min - 1) / 2 + 1)) * 2 + min; //OK
         } 
