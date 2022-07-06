9bad3941153c2ef79ba16d1466819a11af79902a
Natalia Homusko <100041021+homusko007@users.noreply.github.com>
2022-07-06 22:20:02 +0300

    Add files via upload

---
+++ lesson-9/task01.js
@@ -0,0 +1,51 @@
+'use strict';
+
+let items = [];
+
+const cart = {
+    items,
+    totalPrice: 0,
+    count: 0,
+
+    add(el, cost, amount) {
+        items.push({ product: el, price: cost, amount: amount, });
+        this.calculateItemPrice();
+        this.increaseCount(amount);
+    },
+
+    calculateItemPrice() {
+        let sum = 0;
+        for (let i = 0; i < this.items.length; i++) {
+            let elSum = this.items[i].price * this.items[i].amount;
+            sum += elSum;
+        }
+        this.totalPrice = sum;
+        return this.totalPrice;
+    },
+    
+    increaseCount(amount) {
+        cart.count += amount;
+    },
+
+    clear() {
+        cart.items = [];
+        cart.totalPrice = 0;
+        cart.count = 0;
+    },
+
+    print() {
+        const cartStr = JSON.stringify(cart);
+        const cartObj = JSON.parse(cartStr);
+        console.log(cartObj);
+    },
+}
+cart.add('ручка', 20, 1);
+cart.add('карандаш', 15, 5);
+cart.add('тетрадь', 25, 15);
+cart.add('линейка', 10, 1);
+
+console.log(cart);
+
+
+/*items.map(item => item.price).reduce((prev, curr) => prev + curr, 0);
+        return summ;*/
